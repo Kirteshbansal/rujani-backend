a72580828d36c8d5b519073141eafe7e887013ad
Kirtesh Bansal <67135713+Kirteshbansal@users.noreply.github.com>
2023-05-14 17:08:56 +0530

    Add app configs

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 
-// config.js
+// config
 exports.config = {
 	app: {
 		port: process.env.PORT,
 		env: process.env.NODE_ENV,
-		origin: process.env.DEV_ORIGIN,
+		origin: process.env.NODE_ENV === "development" ? process.env.DEV_ORIGIN : process.env.PROD_ORIGIN,
+		appProdFEUrl: process.env.PROD_APP_URL,
+		appDevFEUrl: process.env.DEV_APP_URL,
+		appFEUrl: process.env.NODE_ENV === "development" ? process.env.DEV_APP_URL : process.env.PROD_APP_URL,
 	},
 	db: {
 		databaseURL: process.env.MONGODB_URL,
@@ -13,10 +16,18 @@ exports.config = {
 		username: process.env.MONGODB_USER_NAME,
 	},
 	auth: {
-		accessTokenSecret: process.env.jwtSecret_KEY,
-		accessTokenExpiresIn: process.env.jwtExpiresIn,
-		saltRounds: process.env.SALT_ROUND,
-		refreshTokenSecret: process.env.refreshTokenSecret,
-		refreshTokenExpiresIn: process.env.refreshTokenExpiresIn,
+		accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+		accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
+		saltRounds: +process.env.SALT_ROUND,
+		refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+		refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
+	},
+	email: {
+		appEmail: process.env.EMAIL,
+		emailPassword: process.env.EMAIL_PASSWORD,
+		emailAppPassword: process.env.EMAIL_APP_PASSWORD,
+		emailPort: process.env.EMAIL_PORT,
+		emailHost: process.env.EMAIL_HOST,
+		emailService: process.env.EMAIL_SERVICE,
 	},
 };
